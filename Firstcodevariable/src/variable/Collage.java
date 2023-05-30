72b975236b664aa4c3487d280a8f9d9ccccb1744
pranish9 <pranishtuladhar9@gmail.com>
2023-05-30 10:16:48 +0545

    types of variable

---
+++ Firstcodevariable/src/variable/Collage.java
@@ -0,0 +1,9 @@
+package variable;
+
+public class Collage {
+    int numofcom = 5; // instance variable
+    String  i = "pranish";// instance variable
+    public void method() {
+        System.out.println("the number of computer are " + numofcom);
+    }
+}
---
+++ Firstcodevariable/src/variable/CollageOutput.java
@@ -0,0 +1,11 @@
+package variable;
+
+public class CollageOutput {
+    public static void main(String[] args) {
+        Collage obj = new Collage();
+        obj.method();
+        int a = 10; //local variable
+        System.out.println("name is "+ obj.i + "and the roll no is "+ a );
+    }
+
+}
