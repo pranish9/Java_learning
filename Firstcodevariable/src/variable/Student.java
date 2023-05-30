8d138c6d2dc227cc940e1ad9800b0a9e0f9dbd8d
pranish9 <pranishtuladhar9@gmail.com>
2023-05-30 08:12:20 +0545

    slight change and error is solved

--- Firstcodevariable/src/variable/Student.java
+++ Firstcodevariable/src/variable/Student.java
@@ -4,10 +4,10 @@ package variable;
 public class Student {
 String name = "Virinchi";
     int number= 50; //instance variable
-    static String collageName = "VIRINCHI";    
-    public void methodName {
+
+    public void methodName(){
       
-        System.out.println("sum of the number is " + a);
+        System.out.println("sum of the number is "+ name);
         System.out.println("College name ="+ number);
     }
 }
--- Firstcodevariable/src/variable/StudentOutput.java
+++ Firstcodevariable/src/variable/StudentOutput.java
@@ -3,5 +3,8 @@ package variable;
 public class StudentOutput {
     public static void main(String[] args) {
         Student obj1 = new Student();
+        obj1.methodName();
+        System.out.println("va\tS"+ obj1.number);
+        
     }
 }
