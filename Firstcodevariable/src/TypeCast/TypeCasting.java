925c41b80cde1081d12ce124f8f643d2d63b14ca
pranish9 <pranishtuladhar9@gmail.com>
2023-06-01 07:53:39 +0545

    Type casting demo

---
+++ Firstcodevariable/src/TypeCast/TypeCasting.java
@@ -0,0 +1,12 @@
+package TypeCast;
+
+public class TypeCasting {
+    int a = 5;
+
+    public void wideningcast() {
+        System.out.println("Before type casting ::: "+a);
+        float b = a;
+        System.out.println("after casting :::" + b);
+
+    }
+}
---
+++ Firstcodevariable/src/TypeCast/TypeCastingOut.java
@@ -0,0 +1,8 @@
+package TypeCast;
+
+public class TypeCastingOut {
+    public static void main(String[] args) {
+        TypeCasting obj = new TypeCasting();
+        obj.wideningcast();
+    }
+}
