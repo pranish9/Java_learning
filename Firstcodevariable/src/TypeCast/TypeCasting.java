0f883877f7da10a219fa792cac1228b9ca208ca7
pranish9 <pranishtuladhar9@gmail.com>
2023-06-06 22:04:19 +0545

    casting

--- Firstcodevariable/src/TypeCast/TypeCasting.java
+++ Firstcodevariable/src/TypeCast/TypeCasting.java
@@ -2,14 +2,22 @@ package TypeCast;
 
 public class TypeCasting {
     int a = 5;
+    short c = 5;
 
     public void wideningcast() {
         System.out.println("Before type casting from int to float ::: "+a);
+        int a = 5;
         float b = a;
         System.out.println("after casting from int to float :::" + b);
         System.out.println("::::::::::::::::::::::::::::::::::::::::::");
-        
-
+        System.out.println("Before type casting from short to int ::: "+c);
+        int d = c;
+        System.out.println("after casting from short to int :::" + d);
+        System.out.println("::::::::::::::::::::::::::::::::::::::::::");
+        System.out.println("Before type casting from int to long ::: "+d);
+        float e = d;
+        System.out.println("after casting from int to float :::" + e);
+        System.out.println("::::::::::::::::::::::::::::::::::::::::::");
     }
 
     public void narrowcasting() {
