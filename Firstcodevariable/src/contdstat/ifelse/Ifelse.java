c7d3e5a140fd4014bc690bb986e2e4f664fbbb2b
pranish9 <pranishtuladhar9@gmail.com>
2023-06-06 22:08:04 +0545

    if else statement pratice

--- Firstcodevariable/src/contdstat/ifelse/Ifelse.java
+++ Firstcodevariable/src/contdstat/ifelse/Ifelse.java
@@ -13,4 +13,14 @@ public class Ifelse {
 
         return mathmarks + scienceMarks;
     }
+    public void times(){
+        int time = 22;
+        if (time < 10) {
+            System.out.println("Good morning.");
+        } else if (time < 18) {
+            System.out.println("Good day.");
+        } else {
+            System.out.println("Good evening.");
+        }
+    }
 }
--- Firstcodevariable/src/contdstat/ifelse/IfelseOutput.java
+++ Firstcodevariable/src/contdstat/ifelse/IfelseOutput.java
@@ -6,6 +6,7 @@ public class IfelseOutput {
         Ifelse obj1 = new Ifelse();
         int p = obj1.getMarks();
         System.out.println("the total number is"+ p);
+        obj1.times();
     }
 
 }
