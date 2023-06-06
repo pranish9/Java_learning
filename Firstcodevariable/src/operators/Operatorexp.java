db73153759c334c9e00e89a1fa3e9bbc23627c36
pranish9 <pranishtuladhar9@gmail.com>
2023-06-06 21:33:31 +0545

    operators all done

--- Firstcodevariable/src/operators/Operatorexp.java
+++ Firstcodevariable/src/operators/Operatorexp.java
@@ -9,4 +9,28 @@ public class Operatorexp {
         System.out.println(store);
         System.out.println("addition "+ num1 + " and "+ num2 + " = "+(num1 + num2) );
     }
+
+    public void assign(){
+        int num1 = 1;
+        int num2 = 3;
+
+        num1 += num2;
+        System.out.println("addition of num1 and num2 is" +num1);
+        num2 *= num1;
+        System.out.println("multiplication " +num2);
+    }
+    public void compar(){
+        int i = 10;
+        if ( i == 10){
+            System.out.println("i = 10");
+        }
+
+    }
+    public void logi(){
+        int i = 10;
+        int a = 20;
+        if (i < a && 20 >= a){
+            System.out.println("hence then a = 20 and i = 10");
+        }
+    }
 }
--- Firstcodevariable/src/operators/OperatorexpOutput.java
+++ Firstcodevariable/src/operators/OperatorexpOutput.java
@@ -4,5 +4,8 @@ public class OperatorexpOutput {
     public static void main(String[] args) {
         Operatorexp obj = new Operatorexp();
         obj.Arthimat();
+        obj.compar();
+        obj.logi();
+        obj.assign();
     }
 }
