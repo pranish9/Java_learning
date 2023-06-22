f7be86a9aa9b09dd60903122d18ad399bd701715
pranish9 <pranishtuladhar9@gmail.com>
2023-06-22 08:29:26 +0545

    Static keywords example

---
+++ Firstcodevariable/src/statickeywords/StaticExample.java
@@ -0,0 +1,7 @@
+package statickeywords;
+
+public class StaticExample {
+    public static int getAreaOfCube(int a){
+        return a*a*a;
+    }
+}
---
+++ Firstcodevariable/src/statickeywords/StaticOutput.java
@@ -0,0 +1,9 @@
+package statickeywords;
+
+public class StaticOutput {
+    public static void main(String[] args) {
+
+        System.out.println(StaticExample.getAreaOfCube(5));
+    }
+
+}
