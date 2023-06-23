d470215740d790e79f3375f4f1a6ec3a31ed9dde
pranish9 <pranishtuladhar9@gmail.com>
2023-06-23 08:55:53 +0545

    this keyword learned

---
+++ Firstcodevariable/src/ThisKey/ThisKeyword.java
@@ -0,0 +1,12 @@
+package ThisKey;
+
+public class ThisKeyword {
+    int i ;
+
+    public void setI(int i) {
+        this.i = i;
+    }
+    public void print(){
+        System.out.println(i);
+    }
+}
---
+++ Firstcodevariable/src/ThisKey/ThisKeywordOutput.java
@@ -0,0 +1,9 @@
+package ThisKey;
+
+public class ThisKeywordOutput {
+    public static void main(String[] args) {
+        ThisKeyword obj = new ThisKeyword();
+        obj.setI(5);
+        obj.print();
+    }
+}
