2771081555c4da57856d34236c60fe04b8c50e49
pranish9 <pranishtuladhar9@gmail.com>
2023-06-27 07:54:36 +0545

    constructor in java

---
+++ Firstcodevariable/src/ConstructorExp/ConstructorExample.java
@@ -0,0 +1,12 @@
+package ConstructorExp;
+
+public class ConstructorExample {
+    public ConstructorExample(){
+        System.out.println("Hello from constructor....:::");
+
+    }
+
+    public static void main(String[] args) {
+        ConstructorExample e1 = new ConstructorExample();
+    }
+}
