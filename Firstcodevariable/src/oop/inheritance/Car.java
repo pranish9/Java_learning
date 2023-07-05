0b11bf846140b6140cbc96b96ffc97bebdd1d404
pranish9 <pranishtuladhar9@gmail.com>
2023-07-05 08:07:09 +0545

    using inheritance

---
+++ Firstcodevariable/src/oop/inheritance/Car.java
@@ -0,0 +1,10 @@
+package oop.inheritance;
+
+public class Car extends Vechile {
+    public static void main(String[] args) {
+        Car obj = new Car();
+        obj.displayName();
+    }
+
+
+}
---
+++ Firstcodevariable/src/oop/inheritance/Vechile.java
@@ -0,0 +1,9 @@
+package oop.inheritance;
+
+public class Vechile {
+    public String name = "Bajaj";
+
+    public void displayName() {
+        System.out.println("Ram chandra");
+    }
+}
