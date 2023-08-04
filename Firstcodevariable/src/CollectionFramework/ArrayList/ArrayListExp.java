157cdd8c6940cf37db805a85dbe8e60b73e37cd1
pranish9 <pranishtuladhar9@gmail.com>
2023-08-04 08:11:12 +0545

    Array List example Written

---
+++ Firstcodevariable/src/CollectionFramework/ArrayList/ArrayListExp.java
@@ -0,0 +1,11 @@
+package CollectionFramework.ArrayList;
+import java.util.ArrayList;
+public class ArrayListExp {
+    public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<String>();
+        list.add("Vrinchi");
+        list.add("DAV");
+        list.add("Future Star");
+        System.out.println(list);
+    }
+}
