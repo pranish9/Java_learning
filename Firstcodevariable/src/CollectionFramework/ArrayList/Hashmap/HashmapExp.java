ee8290d7afa0437adb9387a53194cba86c6914bc
pranish9 <pranishtuladhar9@gmail.com>
2023-08-15 07:37:11 +0545

    hashmap and hash set example

---
+++ Firstcodevariable/src/CollectionFramework/ArrayList/Hashmap/HashmapExp.java
@@ -0,0 +1,14 @@
+package CollectionFramework.ArrayList.Hashmap;
+
+import java.util.HashMap;
+
+public class HashmapExp {
+    public void displayHashMap(){
+        HashMap<Integer, String> hm = new HashMap<>();
+        hm.put(111,"Virinchi");
+        hm.put(112,"NCCS");
+        hm.put(113,"KIST");
+        hm.put(114,"NCIT");
+        System.out.println("After Adding data in hashmap:"+hm);
+    }
+}
---
+++ Firstcodevariable/src/CollectionFramework/ArrayList/Hashmap/Hashmapout.java
@@ -0,0 +1,10 @@
+package CollectionFramework.ArrayList.Hashmap;
+
+import CollectionFramework.ArrayList.Hashset.HashsetExamp;
+
+public class Hashmapout {
+    public static void main(String[] args) {
+        HashmapExp hashmapExp = new HashmapExp();
+        hashmapExp.displayHashMap();
+    }
+}
