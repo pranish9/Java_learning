97aea5d12591ebffb920543e4a84f793b904375a
pranish9 <pranishtuladhar9@gmail.com>
2023-08-18 09:33:59 +0545

    CRUD implementation with java

--- Firstcodevariable/src/filehandling/ReadWriteFiles.java
+++ Firstcodevariable/src/filehandling/ReadWriteFiles.java
@@ -1,15 +1,22 @@
 package filehandling;
 
 import java.io.File;
+
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class ReadWriteFiles {
-public void writeFiles(){
+    Scanner sc = new Scanner(System.in);
+    public void writeFiles(){
     File file = new File("D:/java/Firstcodevariable/src/filehandling/test");
     try {
-        FileWriter fileWriter = new FileWriter(file);
-        fileWriter.write("This is just a test for file handling");
+    FileWriter fileWriter = new FileWriter(file);
+//        fileWriter.write("This is just a test for file handling");
+//        fileWriter.close();
+        System.out.println("Enter Text to write on File");
+        String text = sc.nextLine();
+        fileWriter.write(text);
         fileWriter.close();
     } catch (IOException e){
         e.printStackTrace();
